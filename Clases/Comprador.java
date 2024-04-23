e6a610397c312167efa990684d942c798aff89e3
VQ <you@example.com>
2024-04-22 21:21:35 -0400

    Lanzar e imprimir excepciones desde main

--- Clases/Comprador.java
+++ Clases/Comprador.java
@@ -11,16 +11,22 @@ public class Comprador{
 
     //DECLARACIONES DE METODOS
     //Constructor: recibe la moneda con la que comprara, un numero que identifica el tipo de producto y la referencia al Expendedor en el que comprara
-    public Comprador(Moneda moneda, int cualBebida, Expendedor exp){
+    public Comprador(Moneda moneda, int cualBebida, Expendedor exp) throws PagoInsuficienteException, PagoIncorrectoException, NoHayProductoException{
         
-        Producto producto = exp.comprarProducto(moneda,cualBebida);    //Se compra el producto en el expendedor entregandole la moneda y el numero que identifica el tipo de producto
+        Producto producto = null;
+
+        try{
+            producto = exp.comprarProducto(moneda,cualBebida);    //Se compra el producto en el expendedor entregandole la moneda y el numero que identifica el tipo de producto
+        } catch (PagoInsuficienteException | PagoIncorrectoException | NoHayProductoException e){
+            throw e; // Lanzar la expresion nuevamente para que sea manejada en main.
+        }
         
         //Bucle en que se asigna el vuelto 
         while (true) {
             Moneda vueltoexp = exp.getVuelto();                 //Se extrae una moneda del deposito de vuelto del expendedor
             if (vueltoexp == null){break;}                      //Si no se extrajo ninguna moneda se sale del bucle, debido a esto vuelto queda en 0 debido a que no se modifico
             vuelto = vuelto + vueltoexp.getValor();             //Si se logro sacar una moneda, el valor de esa moneda se suma con el vuelto guardado en las iteraciones anteriores
-            if (producto==null) {vuelto = moneda.getValor();break;}    //Si no se logro comprar un producto se asigna el valor de la moneda que uno ingreso, como que el expendedor la devolviera
+            if (producto == null) {vuelto = moneda.getValor();break;}    //Si no se logro comprar un producto se asigna el valor de la moneda que uno ingreso, como que el expendedor la devolviera
         }
 
         //Gesto de consumir el producto
--- Main.java
+++ Main.java
@@ -1,13 +1,64 @@
 import Clases.Monedas.*;    //Importacion de todas las clases de Moneda (100,500,1000,1500)
+import Clases.Monedas.Moneda1000;
 import Clases.*;            //Importacion de todas las clases del paquete Clases (Expendedor,Deposito,Comprador,Producto)
 
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(7);
+        Expendedor exp = null;
         Moneda m = null;
         Comprador c = null;
-        m = new Moneda100();
-        c = new Comprador(m,Expendedor.CONSTANTES.SPRITE.getValor(),exp);
-        System.out.println(c.queConsumiste()+" $"+c.cuantoVuelto());
+
+        // Probar con pago insuficiente
+        try{
+            exp = new Expendedor(3);
+            m = new Moneda100();
+            c = new Comprador(m,Expendedor.CONSTANTES.SPRITE.getValor(),exp);
+            System.out.println(c.queConsumiste()+" $"+c.cuantoVuelto());
+
+        } catch (PagoIncorrectoException e){
+            System.out.println("No se ha ingresado moneda.");
+        } catch(PagoInsuficienteException e){
+            System.out.println("El valor ingresado es insuficiente.");
+        } catch (NoHayProductoException e){
+            System.out.println("No hay producto.");
+        }
+
+        System.out.println("---------------------------------");
+
+        // Probar con moneda nula
+        try{
+            exp = new Expendedor(3);
+            c = new Comprador(m,Expendedor.CONSTANTES.SPRITE.getValor(),exp);
+            System.out.println(c.queConsumiste()+" $"+c.cuantoVuelto());
+
+        } catch (PagoIncorrectoException e){
+            System.out.println("No se ha ingresado moneda.");
+        } catch(PagoInsuficienteException e){
+            System.out.println("El valor ingresado es insuficiente.");
+        } catch (NoHayProductoException e){
+            System.out.println("No hay producto.");
+        }
+
+        System.out.println("---------------------------------");
+
+        // Probar sin producto
+        try{
+            exp = new Expendedor(2);
+
+            Moneda m1 = new Moneda1000();
+            Comprador c1 = new Comprador(m1, Expendedor.CONSTANTES.SPRITE.getValor(), exp);
+
+            m = new Moneda100();
+            c = new Comprador(m,Expendedor.CONSTANTES.SPRITE.getValor(),exp);
+            
+            System.out.println(c.queConsumiste()+" $"+c.cuantoVuelto());
+
+        } catch (PagoIncorrectoException e){
+            System.out.println("No se ha ingresado moneda.");
+        } catch(PagoInsuficienteException e){
+            System.out.println("El valor ingresado es insuficiente.");
+        } catch (NoHayProductoException e){
+            System.out.println("No hay producto.");
+        }
     }
 }
